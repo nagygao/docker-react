481f0a4603abd884561ea2da7955936158480565
nagygao <nagy.gao@gmail.com>
2020-09-30 21:34:42 +0200

    test file modification

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/lHello Joana!/i);
+  const linkElement = getByText(/Hello Joana!/i);
   expect(linkElement).toBeInTheDocument();
 });
